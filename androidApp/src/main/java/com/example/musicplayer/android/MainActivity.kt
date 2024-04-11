6b23c08f1f347f7a0c2a8ed172dc880a12684c15
kkaze <kkazemi640@gmail.com>
2024-04-11 16:32:14 +0400

    Remove default created files by android studio

--- androidApp/src/main/java/com/example/musicplayer/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/musicplayer/android/MainActivity.kt
@@ -8,33 +8,14 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.musicplayer.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
--- shared/src/commonMain/kotlin/com/example/musicplayer/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.example.musicplayer
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
